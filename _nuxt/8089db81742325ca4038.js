e9d6352427db628f607bc124290fbd1c30ca1801
tadaren <tadaren@users.noreply.github.com>
2020-06-02 12:40:46 +0000

    deploy: e711a59986376629509b33b7232f434606c53a33

---
+++ _nuxt/8089db81742325ca4038.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{155:function(t,n,e){"use strict";e.r(n);var c=e(13),component=Object(c.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("Contact")])}),[],!1,null,null,null);n.default=component.exports}}]);
