7d80052359be22b828d5cce4ac645d0866c247d9
tadaren <ptymaato@yahoo.co.jp>
2020-08-27 02:18:04 +0900

    Workの大枠を実装

---
+++ components/Work.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="card" @click="isOpen = true">
+      <div class="card-header">
+        <h4 class="card-header-title is-centered">{{ work.title }}</h4>
+      </div>
+      <div class="card-image">
+        <figure class="image is-16by9">
+          <img :src="work.imagePath" alt />
+        </figure>
+      </div>
+    </div>
+    
+    <b-modal :active.sync="isOpen" :width=640>
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-header-title">{{ work.title }}</h4>
+        </div>
+        <div class="card-image">
+          <figure class="image is-16by9">
+            <img :src="work.imagePath" alt="Image" />
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="content">
+            {{ work.content }}
+          </div>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    work: Object
+  },
+  data() {
+    return {
+      isOpen: false
+    };
+  }
+};
+</script>
--- pages/works.vue
+++ pages/works.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Works</div>
+  <div class="container">
+    <h2 class="title is-2">Works</h2>
+    <ul class="columns is-multiline is-centerd">
+      <li v-for="work in works" :key="work.title" class="column is-4">
+        <work v-bind:work="work"></work>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import Work from '@/components/Work.vue';
+
+export default {
+  components: {
+    Work
+  },
+  data() {
+    return {
+      works: [
+        {
+          title: 'レーザーハープ',
+          content: '何か書く',
+          imagePath: 'work/harp.png'
+        },
+      ]
+    }
+  }
+}
+</script>
